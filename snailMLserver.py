0f127aff0714f9c08f66445068195322ed810571
Vivian Allen <mrvivianallen@gmail.com>
2018-05-04 18:48:35 +0100

    tensorflow references reinstated, wasnt the error

--- snailMLserver.py
+++ snailMLserver.py
@@ -1,6 +1,6 @@
 import os
 import path_helper_main_ml
-# from label_image_no_cli import classify_image
+from label_image_no_cli import classify_image
 from flask import *
 
 
@@ -15,7 +15,7 @@ def upload_file():
     file = request.files['image']
     savepath = os.path.join("./current_image", file.filename)
     file.save(savepath)
-    # move = classify_image('ml/Day4InitialModel', 'Day4InitialModel', savepath)[0][0]
+    move = classify_image('ml/Day4InitialModel', 'Day4InitialModel', savepath)[0][0]
     os.remove(savepath)
     return move
 
