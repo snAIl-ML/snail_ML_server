1dff8b56604180bdf3da4c3a1e7ffb44fb2a1f49
Vivian Allen <mrvivianallen@gmail.com>
2018-05-05 09:45:42 +0100

    test modes (comments) added to get_move_from_server.py

--- get_move_from_server.py
+++ get_move_from_server.py
@@ -11,3 +11,8 @@ def get_server_move(image_path, url):
             output = output + line
     os.remove("response.txt")
     return output
+
+# web test
+#print(get_server_move("test/2018-05-03 18-11-43forwards.jpg", "https://snailapi.herokuapp.com/upload"))
+# local test
+print(get_server_move("test/2018-05-03 18-11-43forwards.jpg", "http://localhost:5000/upload"))
