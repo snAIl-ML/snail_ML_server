062ba69f578f7f4dd28dd74e975ec4e2d4bf8891
Vivian Allen <mrvivianallen@gmail.com>
2018-05-06 09:05:54 +0100

    still cannot test post request

--- get_move_from_server.py
+++ get_move_from_server.py
@@ -1,18 +1,11 @@
 import os
+import requests
 
 def get_server_move(image_path, url):
-    #nb this is acknowledged as terrible, terrible code
-    post_command = ("curl -o response.txt -F 'image=@" +
-    image_path + "' " + url)
-    os.system(post_command)
-    output = ""
-    with open("response.txt") as file:
-        for line in file:
-            output = output + line
-    os.remove("response.txt")
-    return output
+    server_move = requests.post(url, files = {'image': open(image_path, 'rb')})
+    return server_move.text
 
 # web test
-#print(get_server_move("test/2018-05-03 18-11-43forwards.jpg", "https://snailapi.herokuapp.com/upload"))
+print(get_server_move("test/2018-05-03 18-11-43forwards.jpg", "https://snailapi.herokuapp.com/upload"))
 # local test
 print(get_server_move("test/2018-05-03 18-11-43forwards.jpg", "http://localhost:5000/upload"))
--- test/test_snailMLserver.py
+++ test/test_snailMLserver.py
@@ -1,6 +1,7 @@
 import path_helper_test_main
 import unittest
 from snailMLserver import app
+from io import BytesIO
 
 class SnailMLServerTestCase(unittest.TestCase):
 
@@ -10,17 +11,15 @@ class SnailMLServerTestCase(unittest.TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertTrue(b'form action="/upload" method="post"' in response.data)
 
-    def spurious_post_test(self):
-        tester = app.test_client(self)
-        response = tester.post('/upload_test',
-            content_type="text",
-            data={'image': "2018-05-03 18-11-43forwards.jpg"})
-
     def test_uploading_an_image_returns_forwards_pivot_left_or_pivot_right(self):
         tester = app.test_client(self)
+        data = dict(
+            image=(BytesIO(b'my file contents'), "test/2018-05-03 18-11-43forwards.jpg"),
+        )
         response = tester.post('/upload',
-            content_type="multipart/form-data",
-            data={'image': "2018-05-03 18-11-43forwards.jpg"})
+                content_type = 'file',
+                data=data
+            )
         print(response.data)
         self.assertEqual(response.status_code, 200)
         forward_response = b'forwards' in response.data
