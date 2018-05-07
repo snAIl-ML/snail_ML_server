55ff13621fd75ea1d6e55191322b2f86114fd652
Vivian Allen <mrvivianallen@gmail.com>
2018-05-07 07:50:02 +0100

    tests converted to pytest for conformity with main program, feature test for image upload added

--- test/test_snailMLserver.py
+++ test/test_snailMLserver.py
@@ -1,31 +1,19 @@
 import path_helper_test_main
-import unittest
 from snailMLserver import app
-from io import BytesIO
 
-class SnailMLServerTestCase(unittest.TestCase):
+def test_index_page_has_upload_form():
+    tester = app.test_client()
+    response = tester.get('/', content_type='html/text')
+    assert (response.status_code) == 200
+    assert (b'form action="/upload" method="post"' in response.data)
 
-    def test_index_page_has_upload_form(self):
-        tester = app.test_client(self)
-        response = tester.get('/', content_type='html/text')
-        self.assertEqual(response.status_code, 200)
-        self.assertTrue(b'form action="/upload" method="post"' in response.data)
-
-    def test_uploading_an_image_returns_forwards_pivot_left_or_pivot_right(self):
-        tester = app.test_client(self)
-        data = dict(
-            image=(BytesIO(b'my file contents'), "test/2018-05-03 18-11-43forwards.jpg"),
-        )
-        response = tester.post('/upload',
-                content_type = 'file',
-                data=data
-            )
-        print(response.data)
-        self.assertEqual(response.status_code, 200)
-        forward_response = b'forwards' in response.data
-        pivot_left_response = b'pivot_left' in response.data
-        pivot_right_response = b'pivot_right' in response.data
-        self.assertTrue(forward_response or pivot_left_response or pivot_right_response)
-
-if __name__ == "__main__":
-    unittest.main()
+def test_uploading_an_image_returns_forwards_pivot_left_or_pivot_right():
+    #nb test image has to be in root for this test to work, some problem
+    # with the way the filepath is handled...
+    tester = app.test_client()
+    response = tester.post('/upload', data={'image': open('test_image_forwards.jpg', 'rb')})
+    assert (response.status_code) == 200
+    forward_response = b'forward' in response.data
+    pivot_left_response = b'pivot_left' in response.data
+    pivot_right_response = b'pivot_right' in response.data
+    assert (forward_response or pivot_left_response or pivot_right_response)
