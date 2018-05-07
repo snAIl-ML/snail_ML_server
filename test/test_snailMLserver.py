84a9db6f5cb08b5905549b1f82aa4e3d76e9f100
Vivian Allen <mrvivianallen@gmail.com>
2018-05-07 13:12:10 +0100

    system seems functional when intializing ML model to memory instead of loading it with every request. Tested locally with pytest only, no web testdrive

--- test/test_snailMLserver.py
+++ test/test_snailMLserver.py
@@ -17,3 +17,16 @@ def test_uploading_an_image_returns_forwards_pivot_left_or_pivot_right():
     pivot_left_response = b'pivot_left' in response.data
     pivot_right_response = b'pivot_right' in response.data
     assert (forward_response or pivot_left_response or pivot_right_response)
+
+def test_initializing_the_model_into_memory_allows_multiple_requests():
+    tester = app.test_client()
+    response1 = tester.post('/upload', data={'image': open('test_image_forwards.jpg', 'rb')})
+    response2 = tester.post('/upload', data={'image': open('test_image_forwards.jpg', 'rb')})
+    forward_response1 = b'forward' in response1.data
+    pivot_left_response1 = b'pivot_left' in response1.data
+    pivot_right_response1 = b'pivot_right' in response1.data
+    assert (forward_response1 or pivot_left_response1 or pivot_right_response1)
+    forward_response2 = b'forward' in response2.data
+    pivot_left_response2 = b'pivot_left' in response2.data
+    pivot_right_response2 = b'pivot_right' in response2.data
+    assert (forward_response2 or pivot_left_response2 or pivot_right_response2)
