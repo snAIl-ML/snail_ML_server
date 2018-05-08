1b82cb0a7c1773f74cadef217113b08d51f9b096
xelAhcratiPsavilO <alejandropitarcholivas@gmail.com>
2018-05-08 17:29:43 +0100

    server pings snail server with instructions and renders current image, no tests, hardcoded URL

--- snailMLserver.py
+++ snailMLserver.py
@@ -1,25 +1,8 @@
-from flask import Flask, render_template, url_for
-# import os
-# import path_helper_main_ml
-# from label_image_no_cli import initialize_classifier, classify_image
-# from flask import *
-#
-# graph, label = initialize_classifier('ml/train2model', 'training2model')
-#
-# app = Flask(__name__)
-#
-# @app.route("/")
-# def index():
-#     return render_template('index.html')
-#
-# @app.route('/upload', methods=['POST'])
-# def upload_file():
-#     file = request.files['image']
-#     savepath = os.path.join("./current_image", file.filename)
-#     file.save(savepath)
-#     move = classify_image(savepath, graph, label)[0][0]
-#     os.remove(savepath)
-#     return move
+from flask import Flask, render_template, url_for, redirect
+import requests
+
+URL = 'http://192.168.48.195:5000'
+
 from flask import Flask
 app = Flask(__name__)
 
@@ -33,7 +16,9 @@ def self_driven():
 
 @app.route('/rc')
 def user_driven():
-    return render_template('rc.html')
+    html_return = requests.get(URL).text
+    image_url = URL + html_return.split("img src=")[1].split('><')[0]
+    return render_template('rc.html',image_url=image_url)
 
 @app.route('/making_of')
 def tutorials():
@@ -43,5 +28,21 @@ def tutorials():
 def authors():
     return 'You are in the url designated to introduce you to the 4 authors!'
 
+@app.route('/route_left')
+def get_move_left():
+    left_command = requests.get(URL + '/piv_left')
+    print(left_command)
+    return redirect('/rc')
+
+@app.route('/route_forward')
+def get_move_forward():
+    forward_command = requests.get(URL + '/forward')
+    return redirect('/rc')
+
+@app.route('/route_right')
+def get_move_right():
+    right_command = requests.get(URL + '/piv_right')
+    return redirect('/rc')
+
 if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0', port=8000)
