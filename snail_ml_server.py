e3ae9bf9d0e619af4e9535d2a36c6d741aa17471
xelAhcratiPsavilO <alejandropitarcholivas@gmail.com>
2018-05-10 01:46:30 +0100

    Fix all linter violations in snailMLserver.py except E:  5, 0: Unable to import 'classifier' (import-error) as it seems that classifier is operating correctly

--- snail_ml_server.py
+++ snail_ml_server.py
@@ -1,72 +1,71 @@
 'Routes of the server'
 import os
-from classifier import initialize_classifier, classify_image
-from flask import Flask, render_template, redirect, request
 import requests
+from flask import Flask, render_template, redirect, request
+from classifier import initialize_classifier, classify_image
 
-graph, label = initialize_classifier()
+GRAPH, LABEL = initialize_classifier()
 URL = 'http://192.168.49.20:5000'
 
-from flask import Flask
-app = Flask(__name__)
+APP = Flask(__name__)
 
 # USER ROUTES
-@app.route('/')
+@APP.route('/')
 def welcome_page():
     'Main route'
     return render_template('index.html')
 
-@app.route('/ai')
+@APP.route('/ai')
 def self_driven():
     'AI route'
     return 'You are in the url designated to start and stop buttons!'
 
-@app.route('/rc')
+@APP.route('/rc')
 def user_driven():
     'RC route'
     html_return = requests.get(URL).text
     image_url = URL + html_return.split("img src=")[1].split('><')[0]
-    return render_template('rc.html',image_url=image_url)
+    return render_template('rc.html', image_url=image_url)
 
-@app.route('/making_of')
+@APP.route('/making_of')
 def tutorials():
     'Process throughout the project route'
     return 'You are in the url designated to explain how this project made!'
 
-@app.route('/authors')
+@APP.route('/authors')
 def authors():
     'Authors route'
     return 'You are in the url designated to introduce you to the 4 authors!'
 
-@app.route('/route_left')
+@APP.route('/route_left')
 def get_move_left():
     'Move left request'
     requests.get(URL + '/piv_left')
     return redirect('/rc')
 
-@app.route('/route_forward')
+@APP.route('/route_forward')
 def get_move_forward():
     'Move forward request'
     requests.get(URL + '/forward')
     return redirect('/rc')
 
-@app.route('/route_right')
+@APP.route('/route_right')
 def get_move_right():
     'Move right request'
     requests.get(URL + '/piv_right')
     return redirect('/rc')
 
 # API ROUTES
-@app.route('/upload', methods=['POST'])
+@APP.route('/upload', methods=['POST'])
 def upload_file():
     'API routess'
     file = request.files['image']
     savepath = os.path.join("./current_image", file.filename)
     file.save(savepath)
-    move = classify_image(savepath, graph, label)[0][0]
+    move = classify_image(savepath, GRAPH, LABEL)[0][0]
     os.remove(savepath)
     return move
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8000)
+    APP.run(host='0.0.0.0', port=8000)
