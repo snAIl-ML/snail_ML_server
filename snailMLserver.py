3432c4412a7d5b3b22c5d7bc63d135592523e1d5
Vivian Allen <mrvivianallen@gmail.com>
2018-05-10 10:01:10 +0100

    user_driven route now uses session ip variable

--- snailMLserver.py
+++ snailMLserver.py
@@ -26,7 +26,7 @@ def select_mode_page():
 
 @app.route('/rc')
 def user_driven():
-    html_return = requests.get(URL).text
+    html_return = requests.get(session['ip']).text
     image_url = URL + html_return.split("img src=")[1].split('><')[0]
     return render_template('rc.html',image_url=image_url)
 
