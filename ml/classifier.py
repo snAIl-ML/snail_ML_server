bdc27fa17fa288317c12cf193437640a8c353c8a
xelAhcratiPsavilO <alejandropitarcholivas@gmail.com>
2018-05-10 02:37:44 +0100

    Fix all pylint conflicts in classifier.py

--- ml/classifier.py
+++ ml/classifier.py
@@ -12,39 +12,41 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ==============================================================================
-
+'Classifier module'
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-import numpy as np
-import tensorflow as tf
 import os
 import re
+import numpy as np
+import tensorflow as tf
 
 def get_model_name(path="./ml/model"):
+    'Function to get model name'
     model_name = [x for x in os.listdir(path) if re.match(".*(.pb)", x)][0]
     return model_name.split(".pb")[0]
 
 def initialize_classifier(
-    model_name=get_model_name()
+        model_name=get_model_name()
     ):
+    'Function to get classifier'
 
     # load graph
     model_file = "./ml/model/" + model_name + ".pb"
     graph = tf.Graph()
     graph_def = tf.GraphDef()
-    with open(model_file, "rb") as f:
-      graph_def.ParseFromString(f.read())
+    with open(model_file, "rb") as file:
+        graph_def.ParseFromString(file.read())
     with graph.as_default():
-      tf.import_graph_def(graph_def)
+        tf.import_graph_def(graph_def)
 
     # load labels
     label_file = "./ml/model/" + model_name + "_labels.txt"
     label = []
     proto_as_ascii_lines = tf.gfile.GFile(label_file).readlines()
-    for l in proto_as_ascii_lines:
-        label.append(l.rstrip())
+    for lab in proto_as_ascii_lines:
+        label.append(lab.rstrip())
 
     return graph, label
 
@@ -53,8 +55,8 @@ def read_tensor_from_image_file(file_name,
                                 input_width=299,
                                 input_mean=0,
                                 input_std=255):
+    'Function to read tensorflow'
     input_name = "file_reader"
-    output_name = "normalized"
     file_reader = tf.read_file(file_name, input_name)
     if file_name.endswith(".png"):
         image_reader = tf.image.decode_png(
@@ -72,14 +74,15 @@ def read_tensor_from_image_file(file_name,
         )
     float_caster = tf.cast(image_reader, tf.float32)
     dims_expander = tf.expand_dims(float_caster, 0)
-    resized = tf.image.resize_bilinear(dims_expander, [input_height, input_width]) # bet this bit is slow
+    resized = tf.image.resize_bilinear(dims_expander, [input_height, input_width])
     normalized = tf.divide(tf.subtract(resized, [input_mean]), [input_std])
     sess = tf.Session() # does this process close? possible memory leak cause..
     result = sess.run(normalized)
     return result
 
 def classify_image(img_path, graph, labels):
-    t = read_tensor_from_image_file(file_name=img_path)
+    'Function to classify images'
+    tens = read_tensor_from_image_file(file_name=img_path)
     input_name = "import/" + "Placeholder"
     output_name = "import/" + "final_result"
     input_operation = graph.get_operation_by_name(input_name)
@@ -87,9 +90,9 @@ def classify_image(img_path, graph, labels):
 
     with tf.Session(graph=graph) as sess: # this bit is the core of the routine
         results = sess.run(output_operation.outputs[0], {
-            input_operation.outputs[0]: t
+            input_operation.outputs[0]: tens
         })
-    results = np.squeeze(results) # might be trivial, but can cut off everything except the first choice?
+    results = np.squeeze(results) # can cut off everything except the first choice?
 
     top_k = results.argsort()[-5:][::-1]
     final_output = []
