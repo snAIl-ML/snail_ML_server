8156a800487d14d5778005e8b1dec20e6f6ad90a
xelAhcratiPsavilO <alejandropitarcholivas@gmail.com>
2018-05-10 02:43:05 +0100

    Fix all pylint conflicts in test_classifier.py except for import that seems to work

--- ml/test/test_classifier.py
+++ ml/test/test_classifier.py
@@ -1,5 +1,7 @@
-import path_helper_test_ml
+'Module test classifier'
+# Attention pylint stated: E:  3, 0: Unable to import 'classifier' (import-error)
 from classifier import get_model_name
 
-def test_get_model_name_gets_model_name_from_model_folder():
+def test_get_model_name():
+    'test_get_model_name_gets_model_name_from_model_folder'
     assert get_model_name("test/test_model") == 'test_model'
