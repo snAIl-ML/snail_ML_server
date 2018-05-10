ec6c99e48b521d926ba2e34435fa0c24d78ce564
xelAhcratiPsavilO <alejandropitarcholivas@gmail.com>
2018-05-10 01:29:15 +0100

    Fix 10/26  linter violations in snailMLserver.py

--- snailMLserver.py
+++ snailMLserver.py
@@ -1,3 +1,4 @@
+'Routes of the server'
 import os
 import path_helper_main_ml
 from classifier import initialize_classifier, classify_image
@@ -13,45 +14,54 @@ app = Flask(__name__)
 # USER ROUTES
 @app.route('/')
 def welcome_page():
+    'Main route'
     return render_template('index.html')
 
 @app.route('/ai')
 def self_driven():
+    'AI route'
     return 'You are in the url designated to start and stop buttons!'
 
 @app.route('/rc')
 def user_driven():
+    'RC route'
     html_return = requests.get(URL).text
     image_url = URL + html_return.split("img src=")[1].split('><')[0]
     return render_template('rc.html',image_url=image_url)
 
 @app.route('/making_of')
 def tutorials():
+    'Process throughout the project route'
     return 'You are in the url designated to explain how this project made!'
 
 @app.route('/authors')
 def authors():
+    'Authors route'
     return 'You are in the url designated to introduce you to the 4 authors!'
 
 @app.route('/route_left')
 def get_move_left():
+    'Move left request'
     left_command = requests.get(URL + '/piv_left')
     print(left_command)
     return redirect('/rc')
 
 @app.route('/route_forward')
 def get_move_forward():
+    'Move forward request'
     forward_command = requests.get(URL + '/forward')
     return redirect('/rc')
 
 @app.route('/route_right')
 def get_move_right():
+    'Move right request'
     right_command = requests.get(URL + '/piv_right')
     return redirect('/rc')
 
 # API ROUTES
 @app.route('/upload', methods=['POST'])
 def upload_file():
+    'API routes'
     file = request.files['image']
     savepath = os.path.join("./current_image", file.filename)
     file.save(savepath)
