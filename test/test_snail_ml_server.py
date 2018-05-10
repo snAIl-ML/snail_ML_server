f54e3b4a564848c7b7b1181d2ca3285727f5aff0
xelAhcratiPsavilO <alejandropitarcholivas@gmail.com>
2018-05-10 02:03:32 +0100

    Fix all linter violations in test_snail_ml_server.py except probably correct import

--- test/test_snail_ml_server.py
+++ test/test_snail_ml_server.py
@@ -1,16 +1,16 @@
 'Testing routes of the server'
-import path_helper_test_main
+# Attention, linter stating: E:  2, 0: Unable to import 'snail_ml_server' (import-error)
 from snail_ml_server import app
 
 # MAIN INTERFACE TESTS
-def test_index_page_has_x_SPURIOUS_AT_PRESENT():
+def test_index_page():
     'test_index_page_has_x_SPURIOUS_AT_PRESENT'
     tester = app.test_client()
     response = tester.get('/', content_type='html/text')
     assert (response.status_code) == 200
 
 # API TESTS
-def test_uploading_an_image_returns_forwards_pivot_left_or_pivot_right():
+def test_uploading_an_image():
     'test_uploading_an_image_returns_forwards_pivot_left_or_pivot_right'
     #nb test image has to be in root for this test to work, some problem
     # with the way the filepath is handled...
@@ -20,7 +20,7 @@ def test_uploading_an_image_returns_forwards_pivot_left_or_pivot_right():
     assert (response.status_code) == 200
     assert response.data in response_set
 
-def test_image_classifier_allows_multiple_requests():
+def test_image_classifier():
     'test_image_classifier_allows_multiple_requests'
     tester = app.test_client()
     response1 = tester.post('/upload', data={'image': open('test_image_forwards.jpg', 'rb')})
