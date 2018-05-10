c8f21b00bbc32e377d8ee033b7b31eee4f16e66f
xelAhcratiPsavilO <alejandropitarcholivas@gmail.com>
2018-05-10 01:54:40 +0100

    Fix all linter violations in get_move_from_server.py

--- get_move_from_server.py
+++ get_move_from_server.py
@@ -1,13 +1,13 @@
 'Module to get commands for directions'
-import os
 import requests
 
 def get_server_move(image_path, url):
     'Function to obtain command'
-    server_move = requests.post(url, files = {'image': open(image_path, 'rb')})
+    server_move = requests.post(url, files={'image': open(image_path, 'rb')})
     return server_move.text
 
-# web test
-print(get_server_move("test/2018-05-03 18-11-43forwards.jpg", "https://snailapi.herokuapp.com/upload"))
+# web test (uncomment and join the next 2 lines to see the web test (pylint))
+# print(get_server_move("test/2018-05-03 18-11-43forwards.jpg",
+# "https://snailapi.herokuapp.com/upload"))
 # local test
 print(get_server_move("test/2018-05-03 18-11-43forwards.jpg", "http://localhost:5000/upload"))
